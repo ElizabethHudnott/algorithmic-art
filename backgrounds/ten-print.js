6933b379642785abe7e4bec20f29ce3e51873c0c
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2020-03-12 11:39:29 +0000

    Improve 10 PRINT.

--- backgrounds/ten-print.js
+++ backgrounds/ten-print.js
@@ -7,12 +7,13 @@
 class TenPrint {
 
 	constructor() {
-		this.cellsAcrossScreen = 54;
 		this.cellsDownScreen = 25;
+		this.angle = Math.atan(3 / 4);
 		this.zoomOut = 1;
 		this.blankProbability = 0; // 0 <= x <= 0.25 are good values.
 		this.probability = 0.5;
-		this.strokeStyle = '#0088FF';
+		this.stroke1Style = '#887ecb';
+		this.stroke2Style = '#887ecb';
 		// Stroke width as a proportion of the cell's width or height.
 		this.strokeRatio = 0.125;
 	}
@@ -22,68 +23,76 @@ class TenPrint {
 	}
 
 	generate(canvas) {
-		const canvasWidth = canvas.width;
 		const context = canvas.getContext('2d');
 		const zoom = this.zoomOut;
-
-		const widthProportion = canvasWidth / screen.width;
-		let cellsAcrossCanvas = widthProportion * this.cellsAcrossScreen * zoom;
-		const cellWidth = Math.round(canvasWidth / cellsAcrossCanvas);
-		cellsAcrossCanvas = Math.round(canvasWidth / cellWidth);
 
 		const canvasHeight = canvas.height;
 		const heightProportion = canvasHeight / screen.height;
 		let cellsDownCanvas = heightProportion * this.cellsDownScreen * zoom;
-		const cellHeight = Math.round(canvasHeight / cellsDownCanvas);
+		const cellHeight = Math.max(Math.round(canvasHeight / cellsDownCanvas), 2);
 		cellsDownCanvas = Math.round(canvasHeight / cellHeight);
+
+		const cellWidth = Math.max(Math.min(Math.round(cellHeight / Math.tan(this.angle)), 200000), 2);
+		const cellsAcrossCanvas = Math.max(Math.round(canvas.width / cellWidth), 1);
 
 		let lineWidth;
 		if (cellWidth <= cellHeight) {
-			lineWidth = Math.round(this.strokeRatio * cellWidth);
+			lineWidth = Math.round(this.strokeRatio * 2 / 3 * cellWidth);
 		} else {
-			lineWidth = Math.round(this.strokeRatio * cellHeight);
+			lineWidth = Math.round(this.strokeRatio  * 2 / 3 * cellHeight);
 		}
-		if (lineWidth === 0) {
+		if (lineWidth < 1) {
 			lineWidth = 1;
 		}
-		if (lineWidth % 2 === 1) {
-			context.translate(0.5, 0.5);
-		}
 
-		context.beginPath();
-		context.lineWidth = lineWidth;
-		context.lineCap = 'square';
-		context.strokeStyle = this.strokeStyle;
-
-		let blankSpacing = Math.trunc(1 / this.blankProbability);
-		while (blankSpacing > 1 && (cellsAcrossCanvas % blankSpacing <= 1 || cellsAcrossCanvas % blankSpacing === blankSpacing - 1)) {
-			blankSpacing--;
+		let blankSpacing;
+		let blankProbability = this.blankProbability;
+		if (blankProbability > 0) {
+			blankSpacing = Math.trunc(1 / this.blankProbability);
+			while (blankSpacing > 1 && (cellsAcrossCanvas % blankSpacing <= 1 || cellsAcrossCanvas % blankSpacing === blankSpacing - 1)) {
+				blankSpacing--;
+			}
+			blankProbability =  this.blankProbability * blankSpacing;
+		} else {
+			blankSpacing = cellsAcrossCanvas * cellsDownCanvas;
 		}
-		const blankProbability =  this.blankProbability * blankSpacing;
 
 		let cellNumber = 1;
 		for (let cellY = 0; cellY < cellsDownCanvas; cellY++) {
-			const y = cellY * cellHeight;
+			const yTop = cellY * cellHeight;
+			const yBottom = yTop + cellHeight;
 			for (let cellX = 0; cellX < cellsAcrossCanvas; cellX++) {
 				cellNumber++;
 				if (blankSpacing > 0 && cellNumber % blankSpacing === 0 && Math.random() < blankProbability) {
 					continue;
 				}
 
-				const x = cellX * cellWidth;
+				const xLeft = cellX * cellWidth;
+				const xRight = xLeft + cellWidth;
+
 				const p = Math.random();
-				if (p > this.probability) {
-					// Backslash
-					context.moveTo(x, y);
-					context.lineTo(x + cellWidth, y + cellHeight);
-				} else {
+				context.beginPath();
+
+				if (p < this.probability) {
 					// Forward slash
-					context.moveTo(x, y + cellHeight);
-					context.lineTo(x + cellWidth, y);
+					context.fillStyle = this.stroke1Style;
+					context.moveTo(xLeft, yBottom - lineWidth);
+					context.lineTo(xRight, yTop - lineWidth);
+					context.lineTo(xRight, yTop + lineWidth);
+					context.lineTo(xLeft, yBottom + lineWidth);
+					context.closePath();
+				} else {
+					// Backslash
+					context.fillStyle = this.stroke2Style;
+					context.moveTo(xLeft, yTop - lineWidth);
+					context.lineTo(xRight, yBottom - lineWidth);
+					context.lineTo(xRight, yBottom + lineWidth);
+					context.lineTo(xLeft, yTop + lineWidth);
+					context.closePath();
 				}
+				context.fill();
 			}
 		}
-		context.stroke();
 	}
 
 }

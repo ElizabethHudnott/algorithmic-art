573060a23f6a6e43aba83c376f2db2eae721d694
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2021-01-28 23:07:29 +0000

    Implement new 10 PRINT architecture.

--- sketch/ten-print.js
+++ sketch/ten-print.js
@@ -61,6 +61,7 @@ export default function TenPrint() {
 	// Probability of a forward slash given not blank
 	this.probability = 0.5;
 	this.colors = ['#887ecb', '#887ecb', '#887ecb', '#887ecb'];
+	this.numColors = 4;
 	// Stroke width as a proportion of the cell's area.
 	this.strokeRatio = 0.12;
 }
@@ -85,17 +86,9 @@ TenPrint.prototype.generate = function* (context, canvasWidth, canvasHeight, pre
 		cellsDownCanvas = Math.ceil(canvasHeight / cellHeight);
 	}
 
-	const diagonalDist = Math.hypot(canvasWidth, canvasHeight);
-	const style1 = context.createRadialGradient(0, canvasHeight, 0, 0, canvasHeight, diagonalDist);
-	style1.addColorStop(0, this.colors[2]);
-	style1.addColorStop(1, this.colors[1]);
-	const style2 = context.createRadialGradient(canvasWidth, canvasHeight, 0, canvasWidth, canvasHeight, diagonalDist);
-	style2.addColorStop(0, this.colors[3]);
-	style2.addColorStop(1, this.colors[0]);
-
-	const lineWidth = Math.max(Math.round(this.strokeRatio * Math.hypot(cellWidth, cellHeight)) / 2, 0.5);
-	const lineWidth1 = Math.trunc(lineWidth);
-	const lineWidth2 = Math.ceil(lineWidth);
+	const tiles = [new DiagonalLineTile('0'), new DiagonalLineTile('1')];
+	const tileMap = new Array(cellsDownCanvas);
+	const lineWidth = Math.max(Math.round(this.strokeRatio * Math.min(cellWidth, cellHeight)), 1);
 
 	let blankProbability = this.blankProbability;
 	let blankSpacing = blankProbability === 0 ? 0 : Math.max(Math.trunc(1 / blankProbability), 1);
@@ -116,15 +109,14 @@ TenPrint.prototype.generate = function* (context, canvasWidth, canvasHeight, pre
 		}
 		spacingShift = bestSpacingShift;
 	}
-
 	blankProbability =  this.blankProbability * blankSpacing;
 	const maxBlankRun = Math.round(1 / (1 - this.blankProbability) + 0.49) - 1;
 
 	let blankRunLength = 0;
 	let blankDiffusion = 0;
 	for (let cellY = 0; cellY < cellsDownCanvas; cellY++) {
-		const yTop = cellY * cellHeight;
-		const yBottom = yTop + cellHeight;
+		const tileMapRow = new Array(cellsAcrossCanvas);
+		tileMap[cellY] = tileMapRow;
 
 		if (blankSpacing > 1) {
 			blankRunLength = 0;
@@ -137,6 +129,7 @@ TenPrint.prototype.generate = function* (context, canvasWidth, canvasHeight, pre
 				if (blankRunLength < maxBlankRun) {
 					blankRunLength++;
 					blankDiffusion = 0;
+					tileMapRow[cellX] = BlankTile.INSTANCE;
 					continue;
 				} else {
 					blankDiffusion += blankProbability;
@@ -145,34 +138,21 @@ TenPrint.prototype.generate = function* (context, canvasWidth, canvasHeight, pre
 			if (blankDiffusion >= 1 && blankRunLength < maxBlankRun) {
 				blankRunLength++;
 				blankDiffusion--;
+				tileMapRow[cellX] = BlankTile.INSTANCE;
 				continue;
 			}
 
 			blankRunLength = 0;
-			const xLeft = cellX * cellWidth;
-			const xRight = xLeft + cellWidth;
 
 			const p = random.next();
-			context.beginPath();
 
 			if (p < this.probability) {
 				// Forward slash
-				context.fillStyle = style1;
-				context.moveTo(xLeft, yBottom - lineWidth1);
-				context.lineTo(xRight, yTop - lineWidth1);
-				context.lineTo(xRight, yTop + lineWidth2);
-				context.lineTo(xLeft, yBottom + lineWidth2);
-				context.closePath();
+				tileMapRow[cellX] = new Tile(tiles[0]);
 			} else {
 				// Backslash
-				context.fillStyle = style2;
-				context.moveTo(xLeft, yTop - lineWidth1);
-				context.lineTo(xRight, yBottom - lineWidth1);
-				context.lineTo(xRight, yBottom + lineWidth2);
-				context.lineTo(xLeft, yTop + lineWidth2);
-				context.closePath();
+				tileMapRow[cellX] = new Tile(tiles[1]);
 			}
-			context.fill();
 		}
 		unitsProcessed++;
 		if (unitsProcessed >= benchmark) {
@@ -182,4 +162,157 @@ TenPrint.prototype.generate = function* (context, canvasWidth, canvasHeight, pre
 			}
 		}
 	}
+
+	for (let cellY = 0; cellY < cellsDownCanvas; cellY++) {
+		const tileMapRow = tileMap[cellY];
+		for (let cellX = 0; cellX < cellsAcrossCanvas; cellX++) {
+			tileMapRow[cellX].randomizeColors(this.numColors);
+		}
+	}
+
+	const shear = [0, 0, 0, 0];
+	for (let cellY = 0; cellY < cellsDownCanvas; cellY++) {
+		const tileMapRow = tileMap[cellY];
+		const y = cellY * cellHeight;
+		for (let cellX = 0; cellX < cellsAcrossCanvas; cellX++) {
+			const x = cellX * cellWidth;
+			tileMapRow[cellX].draw(context, x, y, cellWidth, cellHeight, lineWidth, shear, this);
+		}
+	}
+}
+
+/*	Locations of port numbers
+	0	1	2	3	4
+	15				5
+	14				6
+	13				7
+	12	11	10	9	8
+ */
+
+class TileType {
+	constructor(connections) {
+		// Maps a port number to an array of port numbers
+		this.connections = connections;
+		for (let [connector, connectees] of Array.from(connections.entries())) {
+			for (let connectee of connectees) {
+				let reverseConnections = connections.get(connectee);
+				if (reverseConnections === undefined) {
+					reverseConnections = [];
+					connections.set(connectee, reverseConnections);
+				}
+				if (!reverseConnections.includes(connector)) {
+					reverseConnections.push(connector);
+				}
+			}
+		}
+	}
+}
+
+class Tile {
+	constructor(tileType) {
+		this.tileType = tileType;
+		this.colors = new Map();
+	}
+
+	flowColor(inPort, color) {
+		if (this.colors.has(inPort)) {
+			return [];
+		}
+		this.colors.set(inPort, color);
+		const outPorts = [];
+		const connected = this.tileType.connections.get(inPort);
+		if (connected !== undefined) {
+			for (let outPort of connected) {
+				if (!this.colors.has(outPort)) {
+					this.colors.set(outPort, color);
+					outPorts.push(outPort);
+				}
+			}
+		}
+		return outPorts;
+	}
+
+	randomizeColors(numColors) {
+		for (let port of this.tileType.connections.keys()) {
+			const colorNum = Math.trunc(Math.random() * numColors);
+			this.colors.set(port, colorNum);
+		}
+	}
+
+	draw(context, x, y, cellWidth, cellHeight, lineWidth, shear, generator) {
+		this.tileType.draw(context, this, x, y, cellWidth, cellHeight, lineWidth, shear, generator);
+	}
+
+}
+
+function coordinateTransform(xReference, yReference, width, height, shear, relativeX, relativeY) {
+	const halfWidth = width / 2;
+	const halfHeight = height / 2;
+	let transformedX, transformedY;
+	if (relativeY <= halfHeight) {
+		transformedX = Math.round(relativeX + relativeY / halfHeight * shear[0]);
+	} else {
+		transformedX = Math.round(relativeX + shear[0] + (relativeY - halfHeight) / halfHeight * shear[1]);
+	}
+	if (relativeX <= halfWidth) {
+		transformedY = Math.round(relativeY + relativeX / halfWidth * shear[2]);
+	} else {
+		transformedY = Math.round(relativeY + shear[2] + (relativeX - halfWidth) / halfWidth * shear[3]);
+	}
+	return [xReference + transformedX, yReference + transformedY];
+}
+
+class BlankTile extends TileType {
+	static INSTANCE = new (function () {
+		this.tileType = new BlankTile();
+		this.flowColor = function () { };
+	});
+
+	constructor() {
+		super(new Map());
+	}
+
+	draw(context, tile, left, top, width, height, lineWidth, shear, generator) {
+		// Nothing to draw
+	}
+}
+
+class DiagonalLineTile extends TileType {
+	constructor(str) {
+		const connections = new Map();
+		if (str === '0') {
+			// Forward slash
+			connections.set(4, [12]);
+		} else {
+			// Backslash
+			connections.set(0, [8]);
+		}
+		super(connections);
+		this.type = str;
+	}
+
+	draw(context, tile, left, top, width, height, lineWidth, shear, generator) {
+		const transform = coordinateTransform.bind(null, left, top, width, height, shear);
+		const lineWidth1 = Math.trunc(lineWidth);
+		const lineWidth2 = Math.ceil(lineWidth);
+		context.beginPath();
+		if (this.type === '0') {
+			// Forward slash
+			context.fillStyle = generator.colors[tile.colors.get(4)];
+			let y = height - lineWidth1;
+			context.moveTo(...transform(0, height - lineWidth1));
+			context.lineTo(...transform(width, -lineWidth1))
+			context.lineTo(...transform(width, lineWidth2));
+			context.lineTo(...transform(0, height + lineWidth2));
+		} else {
+			// Backslash
+			context.fillStyle = generator.colors[tile.colors.get(0)];
+			context.moveTo(...transform(0, -lineWidth1));
+			context.lineTo(...transform(width, height - lineWidth1));
+			context.lineTo(...transform(width, height + lineWidth2));
+			context.lineTo(...transform(0, lineWidth2));
+		}
+		context.fill();
+	}
+
 }
